21e2f45f2b6dc934d0e38888d30318e13f81484b
lekeee <jaleksa388@gmail.com>
2023-12-15 10:34:37 +0100

    dodajem try i catch

--- app/classes/Phone.php
+++ app/classes/Phone.php
@@ -8,54 +8,80 @@ ini_set('display_errors', 1);
 class Phone extends Ad{
     public $accessories;
 
-    public function __construct($user_id = null, $brand = null, $model = null, $title = null, $state = null, $description = null, $images = null, $price = null, $new_price = null, $views = null, $availability = null, $damage = null, $accessories = null) {
-        
-        parent::__construct($user_id, $brand, $model, $title, $state, $description, $images, $price, $new_price, $views, $availability, $damage);
+    public function __construct($user_id = null, $brand = null, $model = null, $title = null, $state = null, $state_rate = null, $description = null, $images = null, $price = null, $new_price = null, $views = null, $availability = null, $damage = null, $accessories = null) {
+        try{
+            parent::__construct($user_id, $brand, $model, $title, $state, $state_rate, $description, $images, $price, $new_price, $views, $availability, $damage);
 
-        $this->accessories = $accessories;
-    }
-
-    public function create($user_id, $brand, $model, $title, $state, $description, $price, $new_price, $images, $availability, $damage, $accessories){
-
-        $sql = "INSERT INTO oglasi (user_id, brand, model, title, state, description, price, new_price, images, availability, damage, accessories)
-                VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
-        $stmt = $this->con->prepare($sql);
-
-        $stmt->bind_param("isssssssssss", $user_id, $brand, $model, $title, $state, $description, $price, $new_price, $images, $availability, $damage, $accessories);
-
-        $result = $stmt->execute();
-        $result = $stmt->affected_rows;
-        
-        return $result > 0 ? true : false;
-    }
-
-    public function saveImages($images, $user_id){
-        $folderName = uniqid($user_id."_"); // Generiše jedinstveno ime foldera za slike
-        $uploadDirectory = "uploads/" . $folderName; // Putanja do foldera za smeštaj slika
-        mkdir($uploadDirectory); // Kreira folder
-
-        foreach ($images['tmp_name'] as $key => $tmp_name) {
-            $originalFileName = basename($images['name'][$key]);
-            $targetFile = $uploadDirectory . '/' . $originalFileName;
-            
-            move_uploaded_file($tmp_name, $targetFile);
+            $this->accessories = $accessories;
+        }
+        catch(Exception $e){
+            throw new OBJECT_NOT_CREATED();
         }
     }
 
+    public function create($user_id, $brand, $model, $title, $state, $state_rate, $description, $price, $new_price, $images, $availability, $damage, $accessories){
+        try{
+            $sql = "INSERT INTO oglasi (user_id, brand, model, title, state, state_rate, description, price, new_price, images, availability, damage, accessories)
+                VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $stmt = $this->con->prepare($sql);
+
+            $stmt->bind_param("issssssssssss", $user_id, $brand, $model, $title, $state, $state_rate, $description, $price, $new_price, $images, $availability, $damage, $accessories);
+
+            $result = $stmt->execute();
+            $result = $stmt->affected_rows;
+            
+            return $result > 0 ? true : false;
+        }
+        catch (Exception $e){
+            throw new AD_CANNOT_BE_CREATED();
+        }
+        
+    }
+
+    public function saveImages($images, $user_id){
+
+        try{
+            $folderName = uniqid($user_id."_"); // Generiše jedinstveno ime foldera za slike
+            $uploadDirectory = "uploads/" . $folderName; // Putanja do foldera za smeštaj slika
+            mkdir($uploadDirectory); // Kreira folder
+
+            foreach ($images['tmp_name'] as $key => $tmp_name) {
+                $originalFileName = basename($images['name'][$key]);
+                $targetFile = $uploadDirectory . '/' . $originalFileName;
+                
+                move_uploaded_file($tmp_name, $targetFile);
+            }
+        }
+        catch(Exception $e){
+            throw new IMAGES_NOT_SAVED();
+        }
+        
+    }
+
     public function select24($offset = 0, $limit = 24){
-        $sql = "SELECT * FROM oglasi LIMIT $limit OFFSET $offset";
-        $result = $this->con->query($sql);
-        return $result->fetch_all(MYSQLI_ASSOC);
+        try{
+            $sql = "SELECT * FROM oglasi LIMIT $limit OFFSET $offset";
+            $result = $this->con->query($sql);
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+        catch(Exception $e){
+            throw new ADS_NOT_SELECTED();
+        }
     }
 
     public function select24UserAds($user_id, $offset = 0, $limit = 24){
-        $sql = "SELECT * FROM oglasi WHERE user_id=? LIMIT $limit OFFSET $offset";
-        $stmt = $this->con->prepare($sql);
-        $stmt->bind_param("i", $ad_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        try{
+            $sql = "SELECT * FROM oglasi WHERE user_id=? LIMIT $limit OFFSET $offset";
+            $stmt = $this->con->prepare($sql);
+            $stmt->bind_param("i", $ad_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        return $result->fetch_all(MYSQLI_ASSOC);
+            return $result->fetch_all(MYSQLI_ASSOC);
+        }
+        catch(Exception $e){
+            throw new ADS_NOT_SELECTED();
+        }
     }
     
     public function read($ad_id){
@@ -75,107 +101,148 @@ class Phone extends Ad{
     }
 
     public function checkIsRated($user_id){
-        $sql = "SELECT * FROM ocene WHERE rater_id=?";
-        $stmt = $this->con->prepare($sql);
-        $stmt->bind_param("i", $user_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        try{
+            $sql = "SELECT * FROM ocene WHERE rater_id=?";
+            $stmt = $this->con->prepare($sql);
+            $stmt->bind_param("i", $user_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        return $result->num_rows == 0 ? false : true;
+            return $result->num_rows == 0 ? false : true;
+        }
+        catch(Exception $e){
+            throw new CHECK_RATE_ERROR();
+        }
     }
 
     public function saveRate($user_id, $ad_id, $ocena){
-        $sql = "INSERT INTO ocene (user_id, ad_id, ocena) 
-                VALUES (?,?,?)";
+        try{
+            $sql = "INSERT INTO ocene (user_id, ad_id, ocena) 
+            VALUES (?,?,?)";
 
             $stmt = $this->con->prepare($sql);
             $stmt->bind_param("iii", $user_id, $ad_id, $ocena);
 
             $stmt->execute();
             $results = $stmt->affected_rows;
-            
-        return $results > 0 ? true : false;
+                
+            return $results > 0 ? true : false;
+        }
+        catch(Exception $e){
+            throw new SAVE_RATE_ERROR();
+        }
+        
     }
 
     public function updateRate($user_id, $ad_id, $ocena){
-        $sql = "UPDATE ocene SET 
+        try{
+            $sql = "UPDATE ocene SET 
                 ocena = ?
                 WHERE user_id = ? AND ad_id=?";
 
-        $stmt = $this->con->prepare($sql);
-        $stmt->bind_param("iii", $ocnea, $user_id, $ad_id);
-        $stmt->execute();
-        $results = $stmt->affected_rows;
+            $stmt = $this->con->prepare($sql);
+            $stmt->bind_param("iii", $ocena, $user_id, $ad_id);
+            $stmt->execute();
+            $results = $stmt->affected_rows;
 
-        return $results > 0 ? true : false;
+            return $results > 0 ? true : false;
+        }
+        catch(Exception $e){
+            throw new UPDATE_RATE_ERROR();
+        }
     }
 
     public function rate($user_id, $ad_id, $ocena){
-        if($this->checkIsRated($user_id)){
-            return $this->updateRate($user_id, $ad_id, $ocena);
+        try{
+            if($this->checkIsRated($user_id)){
+                return $this->updateRate($user_id, $ad_id, $ocena);
+            }
+            else{
+                return $this->saveRate($user_id, $ad_id, $ocena);
+            }
         }
-        else{
-            return $this->saveRate($user_id, $ad_id, $ocena);
+        catch(Exception $e){
+            throw new RATE_ERROR();
         }
     }
 
     public function averageRating($ad_id){
-        $sql = "SELECT ocena FROM ocene WHERE ad_id=?";
-        $stmt = $this->con->prepare($sql);
-        $stmt->bind_param("i", $ad_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        try{
+            $sql = "SELECT ocena FROM ocene WHERE ad_id=?";
+            $stmt = $this->con->prepare($sql);
+            $stmt->bind_param("i", $ad_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        $totalRating = 0;
-        $numberOfRatings = 0;
-    
-        while ($row = $result->fetch_assoc()) {
-            $totalRating += $row['ocena'];
-            $numberOfRatings++;
+            $totalRating = 0;
+            $numberOfRatings = 0;
+        
+            while ($row = $result->fetch_assoc()) {
+                $totalRating += $row['ocena'];
+                $numberOfRatings++;
+            }
+
+            if ($numberOfRatings > 0) {
+                $averageRating = $totalRating / $numberOfRatings;
+                return $averageRating;
+            } else {
+                return 0;
+            }
         }
-
-        if ($numberOfRatings > 0) {
-            $averageRating = $totalRating / $numberOfRatings;
-            return $averageRating;
-        } else {
-            return 0;
+        catch(Exception $e){
+            throw new AVERAGE_RATING_ERROR();
         }
     }
 
     public function checkVisit($ip, $ad_id){
-        $sql = "SELECT * FROM visitors WHERE ip_address=? AND ad_id=?";
-        $stmt = $this->con->prepare($sql);
-        $stmt->bind_param("si", $ip, $ad_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        try{
+            $sql = "SELECT * FROM visitors WHERE ip_address=? AND ad_id=?";
+            $stmt = $this->con->prepare($sql);
+            $stmt->bind_param("si", $ip, $ad_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        return $result->num_rows == 0 ? true : false;
+            return $result->num_rows == 0 ? true : false;
+        }
+        catch(Exception $e){
+            throw new CHECK_VISIT_ERROR();
+        }
     }
 
     public function addVisit($ip, $ad_id){
-        if($this->checkVisit($ip, $ad_id)){
-            $sql = "INSERT INTO visitors (ip_address, ad_id) 
-                VALUES (?,?)";
+        try{
+            if($this->checkVisit($ip, $ad_id)){
+                $sql = "INSERT INTO visitors (ip_address, ad_id) 
+                    VALUES (?,?)";
 
-            $stmt = $this->con->prepare($sql);
-            $stmt->bind_param("si", $ip, $ad_id);
+                $stmt = $this->con->prepare($sql);
+                $stmt->bind_param("si", $ip, $ad_id);
 
-            $stmt->execute();
-            $results = $stmt->affected_rows;
-            
-            return $results > 0 ? true : false;
+                $stmt->execute();
+                $results = $stmt->affected_rows;
+                
+                return $results > 0 ? true : false;
+            }
+            return false;
         }
-        return false;
+        catch(Exception $e){
+            throw new ADD_VISIT_ERROR();
+        }
     }
 
     public function totalVisits($ad_id){
-        $sql = "SELECT COUNT(*) as count FROM visitors WHERE ad_id=?";
-        $stmt = $this->con->prepare($sql);
-        $stmt->bind_param("i", $ad_id); 
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
+        try{
+            $sql = "SELECT COUNT(*) as count FROM visitors WHERE ad_id=?";
+            $stmt = $this->con->prepare($sql);
+            $stmt->bind_param("i", $ad_id); 
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
 
-        return $row['count'];
+            return $row['count'];
+        }
+        catch(Exception $e){
+            throw new TOTAL_VISIT_ERROR();
+        }
     }
 }
--- app/exceptions/adExceptions.php
+++ app/exceptions/adExceptions.php
@@ -1,2 +1,14 @@
 <?php
+class OBJECT_NOT_CREATED extends Exception {}
+class AD_CANNOT_BE_CREATED extends Exception {}
 class AD_CANNOT_BE_READ extends Exception {}
+class IMAGES_NOT_SAVED extends Exception {}
+class ADS_NOT_SELECTED extends Exception {}
+class RATE_ERROR extends Exception {}
+class CHECK_RATE_ERROR extends Exception {}
+class SAVE_RATE_ERROR extends Exception {}
+class UPDATE_RATE_ERROR extends Exception {}
+class AVERAGE_RATING_ERROR extends Exception {}
+class CHECK_VISIT_ERROR extends Exception {}
+class ADD_VISIT_ERROR extends Exception {}
+class TOTAL_VISIT_ERROR extends Exception {}
