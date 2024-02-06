ab4a22a89d839aedd193e2551c06defc7b0b100a
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-02-06 22:07:24 +0100

    Prikazaivanje korisnikovih oglasa (pocetak)

--- public/js/user-profile.js
+++ public/js/user-profile.js
@@ -3,6 +3,7 @@ const userAds = document.querySelector('.user-ads-ident');
 const userInfo = document.querySelector('.user-info-ident');
 const userAdsLink = userAds.querySelector('a');
 const userInfoLink = userInfo.querySelector('a');
+const userPreview = document.querySelector('.user-preview-container');
 
 clickIden.style.width = `${userAds.offsetWidth}px`;
 
@@ -10,10 +11,14 @@ userAdsLink.addEventListener('click', function (e) {
     e.preventDefault();
     clickIden.style.width = `${userAds.offsetWidth}px`;
     clickIden.style.transform = `translateX(0px)`;
+    userPreview.classList.remove('ads');
+    userPreview.classList.add('info');
 });
 
 userInfoLink.addEventListener('click', function (e) {
     e.preventDefault();
     clickIden.style.width = `${userInfo.offsetWidth}px`;
     clickIden.style.transform = `translateX(${userAds.offsetWidth + 20}px)`;
+    userPreview.classList.remove('info');
+    userPreview.classList.add('ads');
 });
