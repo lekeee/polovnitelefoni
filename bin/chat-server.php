9e04c14553c705750f530d599bee4fe0bc85ad8a
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-04-08 22:47:24 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -35,12 +35,12 @@ $server = IoServer::factory(
             new Chat($user, $messages)
         )
     ),
-    443, // Port 443 za HTTPS
+    80, // Port 443 za HTTPS
     '0.0.0.0', // Slušaj sve dostupne IP adrese
     stream_context_create([
         'ssl' => [
-            'local_cert' => '/../ssl/certs/polovni_telefoni_rs_b4b59_a2387_1716933709_083f0ab670ff5a8169bdd1424fb29ea7.crt',
-            'local_pk' => '/../ssl/keys/b4b59_a2387_54abf26b019597734ff123ea1d2fdf22.key',
+            'local_cert' => '../../ssl/certs/www_socket_polovni_telefoni_rs_bc925_3a359_1720381154_4306ad3f9ba75e9d5a03046f9fabca0c.crt',
+            'local_pk' => '.../../ssl/keys/bc925_3a359_6d3ceb2b1330a45f8ad294a214f447f0.key',
             'allow_self_signed' => true, 
             'verify_peer' => false
             // Dodatne SSL opcije po potrebi
