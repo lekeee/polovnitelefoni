a652980defc057e3b1dcd61a1413edde82dd124e
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-04-08 17:39:55 +0200

    Update messagesController.php

--- app/controllers/messagesController.php
+++ app/controllers/messagesController.php
@@ -1,8 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Content-Type: application/json");
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once dirname(__DIR__) . "../config/config.php";
 require_once dirname(__DIR__) . "../classes/Messages.php";
 
