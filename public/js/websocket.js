fda6f719f583bbb3f92e364550676d9ca2dc1bfe
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-04-08 20:10:29 +0200

    Update websocket.js

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,6 +1,6 @@
 let con;
 
-con = new WebSocket(`wss://${document.location.host}:8080?token=${localStorage.getItem('token')}`);
+con = new WebSocket(`wss://${document.location.host}:80?token=${localStorage.getItem('token')}`);
 
 con.onopen = function (e) {
     console.log("uspesna konekcija");
