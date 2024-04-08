b2a82953c6260e693c8f98c40269b986a6327a97
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-04-08 22:56:49 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,16 +1,16 @@
-<?php
-session_abort();
-use Ratchet\Server\IoServer;
-use Ratchet\Http\HttpServer;
-use Ratchet\WebSocket\WsServer;
-use MyApp\Chat;
+// <?php
+// session_abort();
+// use Ratchet\Server\IoServer;
+// use Ratchet\Http\HttpServer;
+// use Ratchet\WebSocket\WsServer;
+// use MyApp\Chat;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
-require dirname(__DIR__) . "/app/config/config.php";
-require dirname(__DIR__) . "/app/auth/userAuthentification.php";
-require dirname(__DIR__) . "/app/classes/Messages.php";
+// require dirname(__DIR__) . '/vendor/autoload.php';
+// require dirname(__DIR__) . "/app/config/config.php";
+// require dirname(__DIR__) . "/app/auth/userAuthentification.php";
+// require dirname(__DIR__) . "/app/classes/Messages.php";
 
-$messages = new Messages();
+// $messages = new Messages();
 
 // $server = IoServer::factory(
 //     new HttpServer(
@@ -29,13 +29,50 @@ $messages = new Messages();
 // ]);
 // $server->run();
 
+// $server = IoServer::factory(
+//     new HttpServer(
+//         new WsServer(
+//             new Chat($user, $messages)
+//         )
+//     ),
+//     80, // Port 443 za HTTPS
+//     '0.0.0.0', // Slušaj sve dostupne IP adrese
+//     stream_context_create([
+//         'ssl' => [
+//             'local_cert' => '/ssl/certs/www_socket_polovni_telefoni_rs_bc925_3a359_1720381154_4306ad3f9ba75e9d5a03046f9fabca0c.crt',
+//             'local_pk' => '/ssl/keys/bc925_3a359_6d3ceb2b1330a45f8ad294a214f447f0.key'
+//             // Dodatne SSL opcije po potrebi
+//         ]
+//     ])
+// );
+
+
+
+<?php
+session_abort();
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\Http\OriginCheck;
+use Ratchet\WebSocket\WsSecureServer; // Promenjeno
+use MyApp\Chat;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . "/app/config/config.php";
+require dirname(__DIR__) . "/app/auth/userAuthentification.php";
+require dirname(__DIR__) . "/app/classes/Messages.php";
+
+$messages = new Messages();
+
 $server = IoServer::factory(
     new HttpServer(
-        new WsServer(
-            new Chat($user, $messages)
+        new OriginCheck(
+            new WsSecureServer( // Promenjeno
+                new Chat($user, $messages)
+            ),
+            ['localhost', '127.0.0.1', 'www.polovni-telefoni.rs'] // Dopusti samo ove origin URL-ove
         )
     ),
-    80, // Port 443 za HTTPS
+    443, // Promenjeno na port 443 za HTTPS
     '0.0.0.0', // Slušaj sve dostupne IP adrese
     stream_context_create([
         'ssl' => [
@@ -45,3 +82,6 @@ $server = IoServer::factory(
         ]
     ])
 );
+
+$server->run();
+
