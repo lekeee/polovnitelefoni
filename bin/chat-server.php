f33a9a046097fd02a23697b9c23cf2b86b6d68e8
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-04-08 22:48:06 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -39,8 +39,8 @@ $server = IoServer::factory(
     '0.0.0.0', // Slušaj sve dostupne IP adrese
     stream_context_create([
         'ssl' => [
-            'local_cert' => '../../ssl/certs/www_socket_polovni_telefoni_rs_bc925_3a359_1720381154_4306ad3f9ba75e9d5a03046f9fabca0c.crt',
-            'local_pk' => '.../../ssl/keys/bc925_3a359_6d3ceb2b1330a45f8ad294a214f447f0.key',
+            'local_cert' => 'www_socket_polovni_telefoni_rs_bc925_3a359_1720381154_4306ad3f9ba75e9d5a03046f9fabca0c.crt',
+            'local_pk' => 'bc925_3a359_6d3ceb2b1330a45f8ad294a214f447f0.key',
             'allow_self_signed' => true, 
             'verify_peer' => false
             // Dodatne SSL opcije po potrebi
