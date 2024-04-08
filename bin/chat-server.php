a3b0314984f7e482f13e0d331dead41f58330271
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-04-08 17:25:26 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -4,22 +4,20 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use MyApp\Chat;
-echo "Pre require";
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 require dirname(__DIR__) . "/app/config/config.php";
 require dirname(__DIR__) . "/app/auth/userAuthentification.php";
 require dirname(__DIR__) . "/app/classes/Messages.php";
-echo "Posle require";
+
 $messages = new Messages();
-echo "Posle messages";
+
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
             new Chat($user, $messages)
         )
     ),
-    9000
+    8080
 );
-echo "Posle server";
 $server->run();
-echo "Posle startovanja";
