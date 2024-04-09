fb4e91c6f105023b49b5cc188b355aba905fade5
lekeee <94479758+lekeee@users.noreply.github.com>
2024-04-09 21:15:16 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,14 +1,19 @@
 <?php
 session_abort();
-use Ratchet\Server\IoServer;
+// use Ratchet\Server\IoServer;
+// use Ratchet\Http\HttpServer;
+// use Ratchet\WebSocket\WsServer;
+// use MyApp\Chat;
+
+// require dirname(__DIR__) . '/vendor/autoload.php';
+// require dirname(__DIR__) . "/app/config/config.php";
+// require dirname(__DIR__) . "/app/auth/userAuthentification.php";
+// require dirname(__DIR__) . "/app/classes/Messages.php";
+
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
+use Ratchet\Server\Proxy;
 use MyApp\Chat;
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-require dirname(__DIR__) . "/app/config/config.php";
-require dirname(__DIR__) . "/app/auth/userAuthentification.php";
-require dirname(__DIR__) . "/app/classes/Messages.php";
 
 $messages = new Messages();
 
