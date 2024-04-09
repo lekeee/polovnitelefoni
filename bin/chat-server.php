95c99553d1c3bc8150bb55fe4c0df2b6914f1b86
lekeee <94479758+lekeee@users.noreply.github.com>
2024-04-09 21:17:56 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -5,10 +5,10 @@ session_abort();
 // use Ratchet\WebSocket\WsServer;
 // use MyApp\Chat;
 
-// require dirname(__DIR__) . '/vendor/autoload.php';
-// require dirname(__DIR__) . "/app/config/config.php";
-// require dirname(__DIR__) . "/app/auth/userAuthentification.php";
-// require dirname(__DIR__) . "/app/classes/Messages.php";
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . "/app/config/config.php";
+require dirname(__DIR__) . "/app/auth/userAuthentification.php";
+require dirname(__DIR__) . "/app/classes/Messages.php";
 
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
@@ -32,29 +32,8 @@ $server->run();
 //     ),
 //     443
 // );
-// $server->socketContext = stream_context_create([
-//     'ssl' => [
-//         'local_cert' => '/ssl/certs/polovni_telefoni_rs_b4b59_a2387_1716933709_083f0ab670ff5a8169bdd1424fb29ea7.crt',
-//         'local_pk' => '/ssl/keys/b4b59_a2387_54abf26b019597734ff123ea1d2fdf22.key',
-//         // Dodatne SSL opcije po potrebi
-//     ]
-// ]);
-$server->run();
 
-// $server = IoServer::factory(
-//     new HttpServer(
-//         new WsServer(
-//             new Chat($user, $messages)
-//         )
-//     ),
-//     443, // Port 443 za HTTPS
-//     '0.0.0.0', // Slušaj sve dostupne IP adrese
-//     stream_context_create([
-//         'ssl' => [
-//             'local_cert' => '/home/polovtel/ssl/certs/www_socket_polovni_telefoni_rs_bc925_3a359_1720381154_4306ad3f9ba75e9d5a03046f9fabca0c.crt',
-//             'local_pk' => '/home/polovtel/ssl/keys/bc925_3a359_6d3ceb2b1330a45f8ad294a214f447f0.key'
-//             // Dodatne SSL opcije po potrebi
-//         ]
-//     ])
-// );
+// $server->run();
+
+
 
