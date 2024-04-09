75bd068fb8a38efe2f1e69db2e55d6f6c5a426e2
lekeee <94479758+lekeee@users.noreply.github.com>
2024-04-09 23:28:28 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,29 +1,29 @@
 
-// session_abort();
-// use Ratchet\Server\IoServer;
-// use Ratchet\Http\HttpServer;
-// use Ratchet\WebSocket\WsServer;
-// use MyApp\Chat;
+session_abort();
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\Chat;
 
-// require dirname(__DIR__) . '/vendor/autoload.php';
-// require dirname(__DIR__) . "/app/config/config.php";
-// require dirname(__DIR__) . "/app/auth/userAuthentification.php";
-// require dirname(__DIR__) . "/app/classes/Messages.php";
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . "/app/config/config.php";
+require dirname(__DIR__) . "/app/auth/userAuthentification.php";
+require dirname(__DIR__) . "/app/classes/Messages.php";
 
-// $messages = new Messages();
+$messages = new Messages();
 
-// $server = IoServer::factory(
-//     new HttpServer(
-//         new WsServer(
-//             new Chat($user, $messages)
-//         )
-//     ),
-//     8080
-// );
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat($user, $messages)
+        )
+    ),
+    443
+);
 
-// $server->run();
+$server->run();
 
-<?php
+<!-- 
 session_abort();
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
@@ -53,4 +53,4 @@ $webServer = new HttpServer(
 
 $server = new IoServer($webServer, $server, $loop);
 
-$server->run();
+$server->run(); -->
