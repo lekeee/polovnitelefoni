b77a6ea1e4f78d225cfc462430c224f9aef48c52
lekeee <94479758+lekeee@users.noreply.github.com>
2024-04-10 12:20:43 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,29 +1,4 @@
 <?php
-session_abort();
-use Ratchet\Server\IoServer;
-use Ratchet\Http\HttpServer;
-use Ratchet\WebSocket\WsServer;
-use MyApp\Chat;
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-require dirname(__DIR__) . "/app/config/config.php";
-require dirname(__DIR__) . "/app/auth/userAuthentification.php";
-require dirname(__DIR__) . "/app/classes/Messages.php";
-
-$messages = new Messages();
-
-$server = IoServer::factory(
-    new HttpServer(
-        new WsServer(
-            new Chat($user, $messages)
-        )
-    ),
-    443
-);
-
-$server->run();
-
-
 // session_abort();
 // use Ratchet\Server\IoServer;
 // use Ratchet\Http\HttpServer;
@@ -35,19 +10,44 @@ $server->run();
 // require dirname(__DIR__) . "/app/auth/userAuthentification.php";
 // require dirname(__DIR__) . "/app/classes/Messages.php";
 
-// $loop = React\EventLoop\Factory::create();
-
 // $messages = new Messages();
 
-// $server = new React\Socket\TcpServer(443);
-// $server = new React\Socket\SecureServer($server, null, array(
-//     'local_cert' => 'home/polovtel/public_html/ssl/polovni_telefoni_rs_b4b59_a2387_1716933709_083f0ab670ff5a8169bdd1424fb29ea7.crt',
-//     'local_pk' => 'home/polovtel/public_html/ssl/b4b59_a2387_54abf26b019597734ff123ea1d2fdf22.key'
-// ));
-// $webServer = new HttpServer(
-//     new WsServer(
-//         new Chat($user, $messages)
-//     )
+// $server = IoServer::factory(
+//     new HttpServer(
+//         new WsServer(
+//             new Chat($user, $messages)
+//         )
+//     ),
+//     443
 // );
-// $server = new IoServer($webServer, $server, $loop);
-// $server->run(); 
+
+// $server->run();
+
+
+session_abort();
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\Chat;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . "/app/config/config.php";
+require dirname(__DIR__) . "/app/auth/userAuthentification.php";
+require dirname(__DIR__) . "/app/classes/Messages.php";
+
+$loop = React\EventLoop\Factory::create();
+
+$messages = new Messages();
+
+$server = new React\Socket\TcpServer(443);
+$server = new React\Socket\SecureServer($server, null, array(
+    'local_cert' => 'home/polovtel/public_html/ssl/certs/polovni_telefoni_rs_b4b59_a2387_1716933709_083f0ab670ff5a8169bdd1424fb29ea7.crt',
+    'local_pk' => 'home/polovtel/public_html/ssl/keys/b4b59_a2387_54abf26b019597734ff123ea1d2fdf22.key'
+));
+$webServer = new HttpServer(
+    new WsServer(
+        new Chat($user, $messages)
+    )
+);
+$server = new IoServer($webServer, $server, $loop);
+$server->run(); 
