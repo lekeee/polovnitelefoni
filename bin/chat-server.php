9f815defc0b08b0e0a304a2fa6947852f7cae810
lekeee <94479758+lekeee@users.noreply.github.com>
2024-04-11 10:25:23 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -13,17 +13,46 @@ require dirname(__DIR__) . "/app/classes/Messages.php";
 
 $messages = new Messages();
 
+// Promenite ovo na adresu i port vašeg proxy servera
+$proxyServerAddress = 'http://localhost/proxy.php'; // Promenite ovo u stvarnu adresu vašeg PHP proxy servera
+$proxyServerPort = 80; // Promenite ovo ako je potrebno, u zavisnosti od port-a koji vaš proxy server koristi
+
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
             new Chat($user, $messages)
         )
     ),
-    443,
+    $proxyServerPort,
     '0.0.0.0'
 );
 
 $server->run();
+
+// session_abort();
+// use Ratchet\Server\IoServer;
+// use Ratchet\Http\HttpServer;
+// use Ratchet\WebSocket\WsServer;
+// use MyApp\Chat;
+
+// require dirname(__DIR__) . '/vendor/autoload.php';
+// require dirname(__DIR__) . "/app/config/config.php";
+// require dirname(__DIR__) . "/app/auth/userAuthentification.php";
+// require dirname(__DIR__) . "/app/classes/Messages.php";
+
+// $messages = new Messages();
+
+// $server = IoServer::factory(
+//     new HttpServer(
+//         new WsServer(
+//             new Chat($user, $messages)
+//         )
+//     ),
+//     443,
+//     '0.0.0.0'
+// );
+
+// $server->run();
 
 
 //     'local_cert' => 'home/polovtel/public_html/ssl/certs/polovni_telefoni_rs_b4b59_a2387_1716933709_083f0ab670ff5a8169bdd1424fb29ea7.crt',
