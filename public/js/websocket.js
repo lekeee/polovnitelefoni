c0fa142fb98e48ee1a0bdd20ac0bff6c83a5d4c6
lekeee <94479758+lekeee@users.noreply.github.com>
2024-04-14 12:02:31 +0200

    Update websocket.js

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,6 +1,6 @@
 let con;
 
-con = new WebSocket(`wss://185.119.89.240:80?token=${localStorage.getItem('token')}`);
+con = new WebSocket(`wss://185.119.89.240:8443?token=${localStorage.getItem('token')}`);
 
 con.onopen = function (e) {
     console.log("uspesna konekcija");
