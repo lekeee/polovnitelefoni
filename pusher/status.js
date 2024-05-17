cc73d47c11df7697af0521bb4ff5f79a90b873e2
lekeee <94479758+lekeee@users.noreply.github.com>
2024-05-17 22:01:37 +0200

    Update status.js

--- pusher/status.js
+++ pusher/status.js
@@ -11,16 +11,21 @@ window.addEventListener('DOMContentLoaded', async function() {
     });;
 });
 
+// window.addEventListener('beforeunload', function() {
+//     fetch('../pusher/pusherController.php', {
+//         method: 'POST',
+//         body: JSON.stringify({action: 'status', status: 'offline' }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         async: false // Ovo je važno kako biste osigurali da se zahtev za ažuriranje statusa korisnika završi pre nego što korisnik napusti stranicu
+//     })
+//     .catch(function(error) {
+//         console.error('Fetch error:', error);
+//     });;
+// });
+
 window.addEventListener('beforeunload', function() {
-    fetch('../pusher/pusherController.php', {
-        method: 'POST',
-        body: JSON.stringify({action: 'status', status: 'offline' }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        async: false // Ovo je važno kako biste osigurali da se zahtev za ažuriranje statusa korisnika završi pre nego što korisnik napusti stranicu
-    })
-    .catch(function(error) {
-        console.error('Fetch error:', error);
-    });;
+    navigator.sendBeacon('../pusher/pusherController.php', JSON.stringify({action: 'status', status: 'offline' }));
 });
+
