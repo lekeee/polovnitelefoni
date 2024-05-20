2e04ec633a2fd830f8be74026a1289ec62d907ad
lekeee <94479758+lekeee@users.noreply.github.com>
2024-05-20 10:15:59 +0200

    Update pusherController.php

--- pusher/pusherController.php
+++ pusher/pusherController.php
@@ -100,6 +100,13 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             );
             echo json_encode($response);
         }
+        else{
+            $response = array(
+                'status' => 'error',
+                'message' => 'Korisnik nije ulogovan'
+            );
+            echo json_encode($response);
+        }
     }
 }
 
